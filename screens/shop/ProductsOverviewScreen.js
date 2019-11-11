de75ec01846515747df4faa089cdcb771b9e4bb8
Eric <ericmares13@gmail.com>
2019-11-11 11:54:05 +0100

    setting up the project

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+
+const ProductsOverviewScreen = props => {
+  return <FlatList />;
+};
+
+export default ProductsOverviewScreen;
