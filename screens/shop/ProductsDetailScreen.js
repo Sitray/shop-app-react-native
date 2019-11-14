82bf9711e62d0db3204ae550da6dd550b894d002
Eric <ericmares13@gmail.com>
2019-11-14 11:58:32 +0100

    showing products on detail screen

--- screens/shop/ProductsDetailScreen.js
+++ screens/shop/ProductsDetailScreen.js
@@ -9,6 +9,8 @@ import {
 } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import Colors from '../../constants/Colors';
+
 const ProductsDetailScreen = props => {
   const productId = props.navigation.getParam('productId');
   const selectedProduct = useSelector(state =>
@@ -16,9 +18,14 @@ const ProductsDetailScreen = props => {
   );
 
   return (
-    <View>
-      <Text>{selectedProduct.title}</Text>
-    </View>
+    <ScrollView>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+      <View style={styles.actions}>
+        <Button color={Colors.primary} title="Add to car" onPress={() => {}} />
+      </View>
+      <Text style={styles.price}>€ {selectedProduct.price.toFixed(2)}</Text>
+      <Text style={styles.description}>{selectedProduct.description}</Text>
+    </ScrollView>
   );
 };
 
@@ -29,7 +36,25 @@ ProductsDetailScreen.navigationOptions = navData => {
 };
 
 const styles = StyleSheet.create({
-  style
+  image: {
+    width: '100%',
+    height: 300
+  },
+  actions: {
+    marginVertical: 10,
+    alignItems: 'center'
+  },
+  price: {
+    fontSize: 20,
+    color: '#888',
+    textAlign: 'center',
+    marginVertical: 20
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'center',
+    marginHorizontal: 20
+  }
 });
 
 export default ProductsDetailScreen;
