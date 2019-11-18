d05d21d02247372c98b45b6309f80022d7b930d5
Eric <ericmares13@gmail.com>
2019-11-18 11:40:57 +0100

    custom font

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -83,12 +83,14 @@ const styles = StyleSheet.create({
     padding: 10
   },
   title: {
+    fontFamily: 'open-sans-bold',
     fontSize: 18,
-    marginVertical: 4
+    marginVertical: 2
   },
   price: {
     fontSize: 14,
-    color: '#888'
+    color: '#888',
+    fontFamily: 'open-sans-bold'
   },
   actions: {
     flexDirection: 'row',
--- screens/shop/ProductsDetailScreen.js
+++ screens/shop/ProductsDetailScreen.js
@@ -48,12 +48,14 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: '#888',
     textAlign: 'center',
-    marginVertical: 20
+    marginVertical: 20,
+    fontFamily: 'open-sans-bold'
   },
   description: {
     fontSize: 14,
     textAlign: 'center',
-    marginHorizontal: 20
+    marginHorizontal: 20,
+    fontFamily: 'open-sans-bold'
   }
 });
 
