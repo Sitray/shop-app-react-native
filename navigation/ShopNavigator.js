2d3ef586960373ac040270b2df2b0737528c4880
Eric <ericmares13@gmail.com>
2019-12-09 12:31:30 +0100

    edit product screen

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -10,6 +10,7 @@ import ProductDetailScreen from '../screens/shop/ProductsDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
 import OrderScreen from '../screens/shop/OrderScreen';
 import UserProductsScreen from '../screens/user/UserProductScreen';
+import EditProductScreen from '../screens/user/EditProductScreen';
 
 import Colors from '../constants/Colors';
 
@@ -66,7 +67,8 @@ const OrdersNavigator = createStackNavigator(
 
 const AdminNavigator = createStackNavigator(
   {
-    UserProducts: UserProductsScreen
+    UserProducts: UserProductsScreen,
+    EditProduct: EditProductScreen
   },
   {
     navigationOptions: {
