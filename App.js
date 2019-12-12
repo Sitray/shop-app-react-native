1a313154b05ba11bae20478c6b85018b8fb34d4c
Eric <ericmares13@gmail.com>
2019-12-12 12:42:29 +0100

    fix import

--- App.js
+++ App.js
@@ -1,28 +1,26 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import productsReducer from './store/reducers/products';
-import cartReducer from './store/reducers/card';
-import orderReducer from './store/reducers/orders';
-import ShopNavigation from './navigation/ShopNavigator';
+import cartReducer from './store/reducers/cart';
+import ordersReducer from './store/reducers/orders';
+import ShopNavigator from './navigation/ShopNavigator';
 
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer,
-  orders: orderReducer
+  orders: ordersReducer
 });
 
 const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-bold.ttf')
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
   });
 };
 
@@ -41,16 +39,7 @@ export default function App() {
   }
   return (
     <Provider store={store}>
-      <ShopNavigation />
+      <ShopNavigator />
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
