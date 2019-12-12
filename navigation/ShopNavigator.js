0b9c30e09aa5cfe3264b17cba752061c8cb1197f
Eric <ericmares13@gmail.com>
2019-12-12 13:02:10 +0100

    added redux thunk and fixing some imports

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import {
-  createStackNavigator,
-  createDrawerNavigator,
-  createAppContainer
-} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
